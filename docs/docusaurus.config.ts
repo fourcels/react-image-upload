8779dc624839eefc6ab5656429427ace58c8fb9a
Kiyan <fourcels@gmail.com>
2024-12-24 15:00:57 +0800

    image baseUrl

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   url: "https://fourcels.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/react-image-upload/",
+  baseUrl: "react-image-upload",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
